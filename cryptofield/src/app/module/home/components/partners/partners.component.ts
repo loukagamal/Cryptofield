681c8b2478b43fb7594de956a0664de33ff3a27a
loukagamal <loukagamal49@gmail.com>
2022-04-15 00:13:31 +0200

    page partners

--- cryptofield/src/app/module/home/components/partners/partners.component.ts
+++ cryptofield/src/app/module/home/components/partners/partners.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+  styleUrls: ['./partners.component.scss'],
 })
 export class PartnersComponent implements OnInit {
+  partners = [
+    { partnersimgurl: 'assets/image/patners-1.svg' },
+    { partnersimgurl: 'assets/image/patners-2.svg' },
+    { partnersimgurl: 'assets/image/patners-3.svg' },
+    { partnersimgurl: 'assets/image/patners-4.svg' },
+    { partnersimgurl: 'assets/image/patners-5.svg' },
+    { partnersimgurl: 'assets/image/patners-6.svg' },
+    { partnersimgurl: 'assets/image/patners-7.svg' },
+    { partnersimgurl: 'assets/image/patners-8.svg' },
+    { partnersimgurl: 'assets/image/patners-9.svg' },
+    { partnersimgurl: 'assets/image/patners-10.svg' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
